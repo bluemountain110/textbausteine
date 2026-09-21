dbed99e8aa898110de123933a94d588adb52603b
bluemountain110 <nadir.tissira@gmail.com>
2026-09-21 13:22:27 +0200

    Fassung 11.0 - Arbeitsplatz gleichziehen (Etappe 5): Fassungsnummer und Cache-Marken

--- speicher.js
+++ speicher.js
@@ -13,7 +13,7 @@
 "use strict";
 window.TB = window.TB || {};
 
-TB.FASSUNG = "10.0 · Etappe 4 abgeschlossen · 20.09.2026";
+TB.FASSUNG = "11.0 · Arbeitsplatz gleichziehen · 20.09.2026";
 
 TB.speicher = (function () {
 
